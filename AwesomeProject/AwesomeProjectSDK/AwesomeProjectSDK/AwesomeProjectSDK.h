be08d03e45312ce957d7faab8ae275760bb63eeb
Tamim Attafi <tamimattafi@iMac-Tamim.local>
2023-07-28 13:11:25 +0700

    config: add ios sdk project

---
+++ AwesomeProject/AwesomeProjectSDK/AwesomeProjectSDK/AwesomeProjectSDK.h
@@ -0,0 +1,18 @@
+//
+//  AwesomeProjectSDK.h
+//  AwesomeProjectSDK
+//
+//  Created by Tamim Attafi on 28.07.2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AwesomeProjectSDK.
+FOUNDATION_EXPORT double AwesomeProjectSDKVersionNumber;
+
+//! Project version string for AwesomeProjectSDK.
+FOUNDATION_EXPORT const unsigned char AwesomeProjectSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AwesomeProjectSDK/PublicHeader.h>
+
+
